added7c3b946cf0c8364e0bbd57ef805aea60fc5
Daniela Tonello <daniela.a.tonello@gmail.com>
2022-07-03 19:06:06 -0300

    header

--- exampleAngular/src/app/app.module.ts
+++ exampleAngular/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoComponent } from './components/header/logo/logo.component';
+import { HeaderComponent } from './components/header/header/header.component';
+import { RedesSocialesComponent } from './components/header/redes-sociales/redes-sociales.component';
+import { LoginComponent } from './components/header/login/login.component';
+import { HeadingComponent } from './components/body/heading/heading.component';
+import { BodyComponent } from './components/body/body/body.component';
+import { BannerComponent } from './components/body/banner/banner.component';
+import { PerfilComponent } from './components/body/perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
+    HeaderComponent,
+    RedesSocialesComponent,
+    LoginComponent,
+    HeadingComponent,
+    BodyComponent,
+    BannerComponent,
+    PerfilComponent,
   ],
   imports: [
     BrowserModule,
